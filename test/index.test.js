f9ea8f3a9c006b95bc256da112bf57f711c74023
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2021-12-12 21:40:59 +0100

    complete step 3

--- test/index.test.js
+++ test/index.test.js
@@ -34,3 +34,34 @@ tap.test('Parse two sentences', test => {
   test.strictSame(actual, expected)
   test.end()
 })
+
+tap.test('Parse four sentences with 2 customer mentions at start', test => {
+  const fourSentencesChat = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.
+14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend.
+14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text.`
+  const expected = [{
+    date: '14:24:32',
+    mention: '14:24:32 Customer : ',
+    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+    type: 'customer'
+  }, {
+    date: '14:27:00',
+    mention: '14:27:00 Customer : ',
+    sentence: 'Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n',
+    type: 'customer'
+  }, {
+    date: '14:27:47',
+    mention: '14:27:47 Agent : ',
+    sentence: 'Vestibulum tempor diam eu leo molestie eleifend.\n',
+    type: 'agent'
+  }, {
+    date: '14:28:28',
+    mention: '14:28:28 Customer : ',
+    sentence: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    type: 'customer'
+  }]
+  const actual = parseChat(fourSentencesChat)
+  test.strictSame(actual, expected)
+  test.end()
+})
