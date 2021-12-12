fef123a7e59f3744d9383a2260253888ddc47993
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2021-12-12 20:17:51 +0100

    complete step 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+'use strict'
+
+const SPACED_COLON = ' : '
+const SPACE = ' '
+
+module.exports = function parseChat(chatText) {
+  const [mention, sentence] = chatText.split(SPACED_COLON)
+  const [date, type] = mention.split(SPACE)
+  return [{
+    date,
+    mention: mention + SPACED_COLON,
+    type: type.toLowerCase(),
+    sentence
+  }]
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,17 @@
 'use strict'
 
 const tap = require('tap')
+const parseChat = require('../index')
 
-tap.test('dummy test', test => {
-  test.strictSame(true, true)
+tap.test('Parse single sentence', test => {
+  const singleSentence = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  const expected = [{
+    date: '14:24:32',
+    mention: '14:24:32 Customer : ',
+    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    type: 'customer'
+  }]
+  const actual = parseChat(singleSentence)
+  test.strictSame(actual, expected)
   test.end()
 })
